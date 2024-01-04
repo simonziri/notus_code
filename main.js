c1c845161178c497fdfaf5fbd799a883fdcb2d21
simonziri <simon@simonziri.com>
2024-01-04 10:31:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const testimonialSlider = new Swiper(".swiper.is-testimonials", {
     slidesPerView: "auto",
-    keyboard: false,
+    keyboard: true,
     speed: 700,
     spaceBetween: "1rem",
     grabCursor: true,
